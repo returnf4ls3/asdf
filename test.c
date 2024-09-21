05ddaa313c0184ff4f22e9b66e75055bf394070c
return_f4ls3 <85391121+returnf4ls3@users.noreply.github.com>
2024-09-21 15:46:51 +0900

    Create test.c

---
+++ test.c
@@ -0,0 +1,110 @@
+#include <stdio.h>
+#include <windows.h>
+
+#define BASEPOS_X 0
+#define BASEPOS_Y 0
+
+void gotoxy(int x, int y) {
+	COORD pos = { x-1+BASEPOS_X, y-1+BASEPOS_Y };
+	SetConsoleCursorPosition(GetStdHandle(STD_OUTPUT_HANDLE), pos);
+}
+
+void drawCup(int centerX, int centerY) {	
+	gotoxy(5+centerX, 1+centerY);
+	printf("___");
+	gotoxy(4+centerX, 2+centerY);
+	printf("/");
+	gotoxy(8+centerX, 2+centerY);
+	printf("\\");
+	gotoxy(3+centerX, 3+centerY);
+	printf("/");
+	gotoxy(9+centerX, 3+centerY);
+	printf("\\");
+	gotoxy(2+centerX, 4+centerY);
+	printf("/");
+	gotoxy(10+centerX, 4+centerY);
+	printf("\\");
+}
+
+void drawCups(int centerX, int centerY, int ans) {			
+	int p = 0;
+
+	system("cls");
+	
+	if (ans != 0) {
+		if (ans == 1) {
+			p = 0;
+			drawCup(centerX+p, centerY-1);
+			p += 11;
+			drawCup(centerX+p, centerY);
+			p += 11;
+			drawCup(centerX+p, centerY);
+			
+			return;
+		} else if (ans == 2) {
+			p = 0;
+			drawCup(centerX+p, centerY);
+			p += 11;
+			drawCup(centerX+p, centerY-1);
+			p += 11;
+			drawCup(centerX+p, centerY);
+			
+			return;
+		} else if (ans == 3) {
+			p = 0;
+			drawCup(centerX+p, centerY);
+			p += 11;
+			drawCup(centerX+p, centerY);
+			p += 11;
+			drawCup(centerX+p, centerY-1);
+			
+			return;
+		}
+	}
+
+	for (int i = 0; i < 3; i++) {
+		drawCup(centerX+p, centerY);
+		p += 11;
+	}
+	
+	gotoxy(1+centerX, 5+centerY);
+	printf("_________________________________\n\n");
+}
+
+// return value is 0 or 1
+int cup() {
+	int selected = 0;
+	int ans = rand() % 3 + 1;
+	
+	drawCups(0, 1, 0);
+	
+	printf("정답: ");
+	scanf("%d", &selected);
+	
+	drawCups(0, 1, selected);
+		
+	if (selected == ans) {
+		return 1;
+	} else {
+		return 0;
+	}
+}
+
+int main() {
+	int a;
+	
+	srand(time(NULL));
+	
+	a = cup(); 
+	
+	if (a == 1) {
+		printf("정답");
+	} else {
+		printf("오답");
+	}
+	
+	//printf("1. 문\n")
+
+	
+	return 0;
+}
