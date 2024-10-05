0805ee5126cb3f7a40837c67e8dc4b20e556805b
return_f4ls3 <85391121+returnf4ls3@users.noreply.github.com>
2024-10-05 10:17:36 +0900

    Update cup.c

--- cup.c
+++ cup.c
@@ -1,37 +1,127 @@
 #include <stdio.h>
 #include <windows.h>
 
+#define BASEPOS_X 0
+#define BASEPOS_Y 0
+
 void gotoxy(int x, int y) {
-	COORD pos = { x-1, y-1 };
+	COORD pos = { x-1+BASEPOS_X, y-1+BASEPOS_Y };
 	SetConsoleCursorPosition(GetStdHandle(STD_OUTPUT_HANDLE), pos);
 }
 
-void drawCup(int centerX, int centerY) {
+void drawCup(int centerX, int centerY) {	
+	gotoxy(5+centerX, 1+centerY);
+	printf("___");
+	gotoxy(4+centerX, 2+centerY);
+	printf("/");
+	gotoxy(8+centerX, 2+centerY);
+	printf("\\");
+	gotoxy(3+centerX, 3+centerY);
+	printf("/");
+	gotoxy(9+centerX, 3+centerY);
+	printf("\\");
+	gotoxy(2+centerX, 4+centerY);
+	printf("/");
+	gotoxy(10+centerX, 4+centerY);
+	printf("\\");
+}
+
+void drawCups(int centerX, int centerY, int sel, int answer) {			
 	int p = 0;
+
+	system("cls");
+	
+	if (sel != 0) {
+		if (sel == 1) {
+			p = 0;
+			if (sel == answer) {
+				drawCup(centerX+p, centerY-1);
+				gotoxy(6+centerX+p, 4+centerY);
+				printf("■");
+			} else {
+				drawCup(centerX+p, centerY-1);
+			}
+			p += 11;
+			drawCup(centerX+p, centerY);
+			p += 11;
+			drawCup(centerX+p, centerY);
+			
+			return;
+		} else if (sel == 2) {
+			p = 0;
+			drawCup(centerX+p, centerY);
+			p += 11;
+			if (sel == answer) {
+				drawCup(centerX+p, centerY-1);
+				gotoxy(6+centerX+p, 4+centerY);
+				printf("■");
+			} else {
+				drawCup(centerX+p, centerY-1);
+			}
+			p += 11;
+			drawCup(centerX+p, centerY);
+			
+			return;
+		} else if (sel == 3) {
+			p = 0;
+			drawCup(centerX+p, centerY);
+			p += 11;
+			drawCup(centerX+p, centerY);
+			p += 11;
+			if (sel == answer) {
+				drawCup(centerX+p, centerY-1);
+				gotoxy(6+centerX+p, 4+centerY);
+				printf("■");
+			} else {
+				drawCup(centerX+p, centerY-1);
+			}
+			
+			return;
+		}
+	}
+
 	for (int i = 0; i < 3; i++) {
-		gotoxy(5+centerX+p, 1+centerY);
-		printf("___");
-		gotoxy(4+centerX+p, 2+centerY);
-		printf("/");
-		gotoxy(8+centerX+p, 2+centerY);
-		printf("\\");
-		gotoxy(3+centerX+p, 3+centerY);
-		printf("/");
-		gotoxy(9+centerX+p, 3+centerY);
-		printf("\\");
-		gotoxy(2+centerX+p, 4+centerY);
-		printf("/");
-		gotoxy(10+centerX+p, 4+centerY);
-		printf("\\");
+		drawCup(centerX+p, centerY);
 		p += 11;
 	}
 	
 	gotoxy(1+centerX, 5+centerY);
-	printf("_________________________________");
+	printf("_________________________________\n\n");
+}
+
+// return value is 0 or 1
+int cup() {
+	int selected = 0;
+	int ans = rand() % 3 + 1;
+	
+	drawCups(0, 1, 0, 0);
+	
+	printf("정답: ");
+	scanf("%d", &selected);
+	
+	drawCups(0, 1, selected, ans);
+		
+	if (selected == ans) {
+		return 1;
+	} else {
+		return 0;
+	}
 }
 
 int main() {
-	drawCup(0, 0);
+	int a;
+	
+	srand(time(NULL));
+	
+	a = cup(); 
+	
+	if (a == 1) {
+		gotoxy(1, 6);
+		printf("정답");
+	} else {
+		gotoxy(1, 6);
+		printf("오답");
+	}
 	
 	return 0;
-} 
+}
